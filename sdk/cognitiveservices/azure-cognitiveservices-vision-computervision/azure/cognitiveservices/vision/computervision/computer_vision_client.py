f875b63969f2f458f5a68538bcedbb6feb33c8fd
Krista Pratico <krpratic@microsoft.com>
2019-09-09 15:49:23 -0700

    docstrings

--- sdk/cognitiveservices/azure-cognitiveservices-vision-computervision/azure/cognitiveservices/vision/computervision/computer_vision_client.py
+++ sdk/cognitiveservices/azure-cognitiveservices-vision-computervision/azure/cognitiveservices/vision/computervision/computer_vision_client.py
@@ -15,11 +15,73 @@ def response_handler(response, deserialized, response_headers):
 
 
 class ComputerVisionClient(ComputerVision):
+    """The Computer Vision API provides state-of-the-art algorithms to process images and return information.
+    For example, it can be used to determine if an image contains mature content, or it can be used to find all the
+    faces in an image.  It also has other features like estimating dominant and accent colors, categorizing the content
+    of images, and describing an image with complete English sentences.  Additionally, it can also intelligently
+    generate images thumbnails for displaying large images effectively.
 
+    :param endpoint: Supported Cognitive Services endpoints.
+    :type endpoint: str
+    :param credentials: Cognitive Services account key credentials needed for the client to connect to Azure.
+    :type credentials: str
+    """
     def __init__(self, endpoint, credentials, **kwargs):
         super(ComputerVisionClient, self).__init__(endpoint=endpoint, credentials=credentials, **kwargs)
 
     def analyze_image(self, data, visual_features=None, details=None, language="en", description_exclude=None, **kwargs):
+        """This operation extracts a rich set of visual features based on the
+        image content.
+        Two input methods are supported -- (1) Uploading an image or (2)
+        specifying an image URL. Within your request, there is an optional
+        parameter to allow you to choose which features to return. By default,
+        image categories are returned in the response.
+        A successful response will be returned in JSON. If the request failed,
+        the response will contain an error code and a message to help
+        understand what went wrong.
+
+        :param data: Publicly reachable URL of an image or an image stream.
+        :type data: str or bytes
+        :param visual_features: A string indicating what visual feature types
+         to return. Multiple values should be comma-separated. Valid visual
+         feature types include: Categories - categorizes image content
+         according to a taxonomy defined in documentation. Tags - tags the
+         image with a detailed list of words related to the image content.
+         Description - describes the image content with a complete English
+         sentence. Faces - detects if faces are present. If present, generate
+         coordinates, gender and age. ImageType - detects if image is clipart
+         or a line drawing. Color - determines the accent color, dominant
+         color, and whether an image is black&white. Adult - detects if the
+         image is pornographic in nature (depicts nudity or a sex act), or is
+         gory (depicts extreme violence or blood). Sexually suggestive content
+         (aka racy content) is also detected. Objects - detects various objects
+         within an image, including the approximate location. The Objects
+         argument is only available in English. Brands - detects various brands
+         within an image, including the approximate location. The Brands
+         argument is only available in English.
+        :type visual_features: list[str or
+         ~computervision.models.VisualFeatureTypes]
+        :param details: A string indicating which domain-specific details to
+         return. Multiple values should be comma-separated. Valid visual
+         feature types include: Celebrities - identifies celebrities if
+         detected in the image, Landmarks - identifies notable landmarks in the
+         image.
+        :type details: list[str or ~computervision.models.Details]
+        :param language: The desired language for output generation. If this
+         parameter is not specified, the default value is
+         &quot;en&quot;.Supported languages:en - English, Default. es -
+         Spanish, ja - Japanese, pt - Portuguese, zh - Simplified Chinese.
+         Possible values include: 'en', 'es', 'ja', 'pt', 'zh'
+        :type language: str
+        :param description_exclude: Turn off specified domain models when
+         generating the description.
+        :type description_exclude: list[str or
+         ~computervision.models.DescriptionExclude]
+        :return: ImageAnalysis
+        :rtype: ~computervision.models.ImageAnalysis
+        :raises:
+         :class:`ComputerVisionErrorException<computervision.models.ComputerVisionErrorException>`
+        """
         if isinstance(data, six.text_type):
             try:
                 return self.vision.analyze_image(
@@ -50,6 +112,38 @@ class ComputerVisionClient(ComputerVision):
             raise TypeError("Unsupported data type: {}".format(type(data)))
 
     def describe_image(self, url, max_candidates=1, language="en", description_exclude=None, **kwargs):
+        """This operation generates a description of an image in human readable
+        language with complete sentences. The description is based on a
+        collection of content tags, which are also returned by the operation.
+        More than one description can be generated for each image. Descriptions
+        are ordered by their confidence score. Descriptions may include results
+        from celebrity and landmark domain models, if applicable.
+        Two input methods are supported -- (1) Uploading an image or (2)
+        specifying an image URL.
+        A successful response will be returned in JSON. If the request failed,
+        the response will contain an error code and a message to help
+        understand what went wrong.
+
+        :param data: Publicly reachable URL of an image or an image stream.
+        :type data: str or bytes
+        :param max_candidates: Maximum number of candidate descriptions to be
+         returned.  The default is 1.
+        :type max_candidates: int
+        :param language: The desired language for output generation. If this
+         parameter is not specified, the default value is
+         &quot;en&quot;.Supported languages:en - English, Default. es -
+         Spanish, ja - Japanese, pt - Portuguese, zh - Simplified Chinese.
+         Possible values include: 'en', 'es', 'ja', 'pt', 'zh'
+        :type language: str
+        :param description_exclude: Turn off specified domain models when
+         generating the description.
+        :type description_exclude: list[str or
+         ~computervision.models.DescriptionExclude]
+        :return: ImageDescription
+        :rtype: ~computervision.models.ImageDescription
+        :raises:
+         :class:`ComputerVisionErrorException<computervision.models.ComputerVisionErrorException>`
+        """
         try:
             return self.vision.describe_image(
                 url=url,
@@ -63,6 +157,20 @@ class ComputerVisionClient(ComputerVision):
             raise error
 
     def detect_objects(self, url, **kwargs):
+        """Performs object detection on the specified image.
+        Two input methods are supported -- (1) Uploading an image or (2)
+        specifying an image URL.
+        A successful response will be returned in JSON. If the request failed,
+        the response will contain an error code and a message to help
+        understand what went wrong.
+
+        :param data: Publicly reachable URL of an image or an image stream.
+        :type data: str or bytes
+        :return: DetectResult
+        :rtype: ~computervision.models.DetectResult
+        :raises:
+         :class:`ComputerVisionErrorException<computervision.models.ComputerVisionErrorException>`
+        """
         try:
             return self.vision.detect_objects(
                 url=url,
@@ -73,6 +181,19 @@ class ComputerVisionClient(ComputerVision):
             raise error
 
     def list_models(self, **kwargs):
+        """This operation returns the list of domain-specific models that are
+        supported by the Computer Vision API. Currently, the API supports
+        following domain-specific models: celebrity recognizer, landmark
+        recognizer.
+        A successful response will be returned in JSON. If the request failed,
+        the response will contain an error code and a message to help
+        understand what went wrong.
+
+        :return: ListModelsResult
+        :rtype: ~computervision.models.ListModelsResult
+        :raises:
+         :class:`ComputerVisionErrorException<computervision.models.ComputerVisionErrorException>`
+        """
         try:
             return self.vision.list_models(
                 cls=kwargs.pop("cls", None),
@@ -82,6 +203,32 @@ class ComputerVisionClient(ComputerVision):
             raise error
 
     def analyze_image_by_domain(self, model, url, language="en", **kwargs):
+        """This operation recognizes content within an image by applying a
+        domain-specific model. The list of domain-specific models that are
+        supported by the Computer Vision API can be retrieved using the /models
+        GET request. Currently, the API provides following domain-specific
+        models: celebrities, landmarks.
+        Two input methods are supported -- (1) Uploading an image or (2)
+        specifying an image URL.
+        A successful response will be returned in JSON.
+        If the request failed, the response will contain an error code and a
+        message to help understand what went wrong.
+
+        :param data: Publicly reachable URL of an image or an image stream.
+        :type data: str or bytes
+        :param model: The domain-specific content to recognize.
+        :type model: str
+        :param language: The desired language for output generation. If this
+         parameter is not specified, the default value is
+         &quot;en&quot;.Supported languages:en - English, Default. es -
+         Spanish, ja - Japanese, pt - Portuguese, zh - Simplified Chinese.
+         Possible values include: 'en', 'es', 'ja', 'pt', 'zh'
+        :type language: str
+        :return: DomainModelResults
+        :rtype: ~computervision.models.DomainModelResults
+        :raises:
+         :class:`ComputerVisionErrorException<computervision.models.ComputerVisionErrorException>`
+        """
         try:
             return self.vision.analyze_image_by_domain(
                 model=model,
@@ -94,6 +241,33 @@ class ComputerVisionClient(ComputerVision):
             raise error
 
     def recognize_printed_text(self, url, detect_orientation=True, language="unk", **kwargs):
+        """Optical Character Recognition (OCR) detects text in an image and
+        extracts the recognized characters into a machine-usable character
+        stream.
+        Upon success, the OCR results will be returned.
+        Upon failure, the error code together with an error message will be
+        returned. The error code can be one of InvalidImageUrl,
+        InvalidImageFormat, InvalidImageSize, NotSupportedImage,
+        NotSupportedLanguage, or InternalServerError.
+
+        :param data: Publicly reachable URL of an image or an image stream.
+        :type data: str or bytes
+        :param detect_orientation: Whether detect the text orientation in the
+         image. With detectOrientation=true the OCR service tries to detect the
+         image orientation and correct it before further processing (e.g. if
+         it's upside-down).
+        :type detect_orientation: bool
+        :param language: The BCP-47 language code of the text to be detected
+         in the image. The default value is 'unk'. Possible values include:
+         'unk', 'zh-Hans', 'zh-Hant', 'cs', 'da', 'nl', 'en', 'fi', 'fr', 'de',
+         'el', 'hu', 'it', 'ja', 'ko', 'nb', 'pl', 'pt', 'ru', 'es', 'sv',
+         'tr', 'ar', 'ro', 'sr-Cyrl', 'sr-Latn', 'sk'
+        :type language: str or ~computervision.models.OcrLanguages
+        :return: OcrResult
+        :rtype: ~computervision.models.OcrResult
+        :raises:
+         :class:`ComputerVisionErrorException<computervision.models.ComputerVisionErrorException>`
+        """
         try:
             return self.vision.recognize_printed_text(
                 url=url,
@@ -106,6 +280,32 @@ class ComputerVisionClient(ComputerVision):
             raise error
 
     def tag_image(self, url, language="en", **kwargs):
+        """This operation generates a list of words, or tags, that are relevant to
+        the content of the supplied image. The Computer Vision API can return
+        tags based on objects, living beings, scenery or actions found in
+        images. Unlike categories, tags are not organized according to a
+        hierarchical classification system, but correspond to image content.
+        Tags may contain hints to avoid ambiguity or provide context, for
+        example the tag "ascomycete" may be accompanied by the hint "fungus".
+        Two input methods are supported -- (1) Uploading an image or (2)
+        specifying an image URL.
+        A successful response will be returned in JSON. If the request failed,
+        the response will contain an error code and a message to help
+        understand what went wrong.
+
+        :param data: Publicly reachable URL of an image or an image stream.
+        :type data: str or bytes
+        :param language: The desired language for output generation. If this
+         parameter is not specified, the default value is
+         &quot;en&quot;.Supported languages:en - English, Default. es -
+         Spanish, ja - Japanese, pt - Portuguese, zh - Simplified Chinese.
+         Possible values include: 'en', 'es', 'ja', 'pt', 'zh'
+        :type language: str
+        :return: TagResult
+        :rtype: ~computervision.models.TagResult
+        :raises:
+         :class:`ComputerVisionErrorException<computervision.models.ComputerVisionErrorException>`
+        """
         try:
             return self.vision.tag_image(
                 url=url,
@@ -117,6 +317,33 @@ class ComputerVisionClient(ComputerVision):
             raise error
 
     def generate_thumbnail(self, width, height, url, smart_cropping=False, **kwargs):
+        """This operation generates a thumbnail image with the user-specified
+        width and height. By default, the service analyzes the image,
+        identifies the region of interest (ROI), and generates smart cropping
+        coordinates based on the ROI. Smart cropping helps when you specify an
+        aspect ratio that differs from that of the input image.
+        A successful response contains the thumbnail image binary. If the
+        request failed, the response contains an error code and a message to
+        help determine what went wrong.
+        Upon failure, the error code and an error message are returned. The
+        error code could be one of InvalidImageUrl, InvalidImageFormat,
+        InvalidImageSize, InvalidThumbnailSize, NotSupportedImage,
+        FailedToProcess, Timeout, or InternalServerError.
+
+        :param data: Publicly reachable URL of an image or an image stream.
+        :type data: str or bytes
+        :param width: Width of the thumbnail, in pixels. It must be between 1
+         and 1024. Recommended minimum of 50.
+        :type width: int
+        :param height: Height of the thumbnail, in pixels. It must be between
+         1 and 1024. Recommended minimum of 50.
+        :type height: int
+        :param smart_cropping: Boolean flag for enabling smart cropping.
+        :type smart_cropping: bool
+        :return: Generator
+        :rtype: ~azure.core.pipeline.transport.requests_basic.StreamDownloadGenerator
+        :raises: :class:`HttpResponseError<azure.core.HttpResponseError>`
+        """
         try:
             return self.vision.generate_thumbnail(
                 width=width,
@@ -130,6 +357,23 @@ class ComputerVisionClient(ComputerVision):
             raise error
 
     def get_area_of_interest(self, url, **kwargs):
+        """This operation returns a bounding box around the most important area of
+        the image.
+        A successful response will be returned in JSON. If the request failed,
+        the response contains an error code and a message to help determine
+        what went wrong.
+        Upon failure, the error code and an error message are returned. The
+        error code could be one of InvalidImageUrl, InvalidImageFormat,
+        InvalidImageSize, NotSupportedImage, FailedToProcess, Timeout, or
+        InternalServerError.
+
+        :param data: Publicly reachable URL of an image or an image stream.
+        :type data: str or bytes
+        :return: AreaOfInterestResult
+        :rtype: ~computervision.models.AreaOfInterestResult
+        :raises:
+         :class:`ComputerVisionErrorException<computervision.models.ComputerVisionErrorException>`
+        """
         try:
             return self.vision.get_area_of_interest(
                 url=url,
@@ -242,6 +486,18 @@ class ComputerVisionClient(ComputerVision):
             raise error
 
     def recognize_text(self, mode, url, **kwargs):
+        """Recognize Text operation.
+
+        :param data: Publicly reachable URL of an image or an image stream.
+        :type data: str or bytes
+        :param mode: Type of text to recognize. Possible values include:
+         'Handwritten', 'Printed'
+        :type mode: str or ~ocr.models.TextRecognitionMode
+        :return: A poller object
+        :rtype: ~azure.core.polling.LROPoller
+        :raises:
+         :class:`ComputerVisionErrorException<ocr.models.ComputerVisionErrorException>`
+        """
         try:
             job = self.vision.recognize_text(
                 mode=mode,
@@ -267,6 +523,17 @@ class ComputerVisionClient(ComputerVision):
         return LROPoller(command, start_recognize_text, None, polling_method)
 
     def batch_read_file(self, url, **kwargs):
+        """Use this interface to get the result of a Read operation, employing the
+        state-of-the-art Optical Character Recognition (OCR) algorithms
+        optimized for text-heavy documents.
+
+        :param data: Publicly reachable URL of an image or an image stream.
+        :type data: str or bytes
+        :return: A poller object
+        :rtype: ~azure.core.polling.LROPoller
+        :raises:
+         :class:`ComputerVisionErrorException<ocr.models.ComputerVisionErrorException>`
+        """
         try:
             job = self.vision.batch_read_file(
                 url=url,

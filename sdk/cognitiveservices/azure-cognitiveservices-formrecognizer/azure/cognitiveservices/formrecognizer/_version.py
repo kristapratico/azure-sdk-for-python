7de326863130d5b96e6c5e7bc7679f86a9c3e81b
Krista Pratico <krpratic@microsoft.com>
2020-02-07 10:38:11 -0800

    add v3 generation + some handwritten layer

--- sdk/cognitiveservices/azure-cognitiveservices-formrecognizer/azure/cognitiveservices/formrecognizer/_version.py
+++ sdk/cognitiveservices/azure-cognitiveservices-formrecognizer/azure/cognitiveservices/formrecognizer/_version.py
@@ -4,8 +4,4 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-from ._form_recognizer_client_operations import FormRecognizerClientOperationsMixin
-
-__all__ = [
-    'FormRecognizerClientOperationsMixin',
-]
+VERSION = "2.0.0"

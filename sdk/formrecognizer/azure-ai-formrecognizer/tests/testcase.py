ab19f0dce3eb0f7a6247ff372204eb93f382403f
Krista Pratico <krpratic@microsoft.com>
2021-01-26 17:57:44 -0800

    try

--- sdk/formrecognizer/azure-ai-formrecognizer/tests/testcase.py
+++ sdk/formrecognizer/azure-ai-formrecognizer/tests/testcase.py
@@ -35,7 +35,7 @@ from azure_devtools.scenario_tests.utilities import is_text_payload
 
 LOGGING_FORMAT = '%(asctime)s %(name)-20s %(levelname)-5s %(message)s'
 ENABLE_LOGGER = os.getenv('ENABLE_LOGGER', "False")
-REGION = os.getenv('LOCATION', None)
+REGION = os.getenv('REGION', None)
 ENDPOINT = os.getenv('ENDPOINT', 'None')
 NAME = os.getenv('NAME', 'None')
 RESOURCE_GROUP = os.getenv('RESOURCE_GROUP', 'None')
